e1aa136e31cbfc7c08d8fab94ee836c03cbf2266
prititi <pt446914@gamil.com>
2022-10-19 17:14:20 +0530

    prime

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 let num=13;
 let N=0;
-  for(let i=0; i<=num; i++){
+  for(let i=1; i<=num; i++){
       if(num%i===0){
           N+=1;
       }
   }
-if(N<=5){
+if(N<=2){
     console.log("Yes");
 }
 else{
